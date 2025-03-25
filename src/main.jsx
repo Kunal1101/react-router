a03a24d897d426598547cfe0a9c7bbfcdc66f7f5
Kunal Mishra <kunalmishra8127@gmail.com>
2025-03-25 14:51:26 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout";
+import About from "./components/About";
+import User from "./components/User";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
